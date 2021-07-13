9981fd3bda47d06a4459ae9f0067fa29d7700899
Matheus Oliveira <m.loliveira@alunos.fho.edu.br>
2021-07-13 09:32:43 -0300

    fix tratamento de rotas para retorno 404

--- index.php
+++ index.php
@@ -6,10 +6,16 @@ require __DIR__ . '/vendor/autoload.php';
 include "routes.php";
 include "helpers.php";
 
-if(!isset($routes[$_SERVER['REQUEST_URI']])) {
+$url = parse_url($_SERVER['REQUEST_URI']);
+
+if(!isset($routes[$url['path']])) {
   http_response_code(404);
   echo '404';
   die();
 }
 
-$routes[$_SERVER['REQUEST_URI']]();
+if($_SERVER['REQUEST_METHOD'] != "GET") {
+  $_POST = json_decode(file_get_contents("php://input"));
+}
+
+$routes[$url['path']]();
