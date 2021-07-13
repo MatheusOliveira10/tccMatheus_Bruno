54f6628c46b6266cafb372fe34011f8abdeb9ead
Matheus Oliveira <m.loliveira@alunos.fho.edu.br>
2021-07-13 09:33:09 -0300

    rotas para todos os controllers

--- routes.php
+++ routes.php
@@ -19,18 +19,88 @@ function verificaMetodo($metodoCorreto) {
 }
 
 $routes = [
-    "/controlador" => function() use ($controlador) {
-        verificaMetodo(["GET", "POST"]);
+    "/controladores" => function() use ($controlador) {
+        verificaMetodo(["GET", "POST", "PUT", "DELETE"]);
 
-        $_SERVER['REQUEST_METHOD'] == 'GET'
-            ? $controlador->index()
-            : $controlador->store($_POST);
+        switch($_SERVER['REQUEST_METHOD']) {
+            case 'GET':
+                $controlador->index();
+                break;
+
+            case 'POST':
+                $controlador->store($_POST);
+                break;
+
+            case 'PUT':
+                $controlador->update($_POST);
+                break;
+
+            case 'DELETE':
+                $controlador->delete($_POST);
+                break;    
+        } 
     },
-    "/cultura" => function() use ($cultura) {
-        verificaMetodo(["GET", "POST"]);
+    "/culturas" => function() use ($cultura) {
+        verificaMetodo(["GET", "POST", "PUT", "DELETE"]);
+        
+        switch($_SERVER['REQUEST_METHOD']) {
+            case 'GET':
+                $cultura->index();
+                break;
 
-        $_SERVER['REQUEST_METHOD'] == 'GET'
-            ? $cultura->index()
-            : $cultura->store($_POST);
+            case 'POST':
+                $cultura->store($_POST);
+                break;
+
+            case 'PUT':
+                $cultura->update($_POST);
+                break;
+
+            case 'DELETE':
+                $cultura->delete($_POST);
+                break;    
+        } 
+    },
+    "/sensores" => function() use ($sensor) {
+        verificaMetodo(["GET", "POST", "PUT", "DELETE"]);
+        
+        switch($_SERVER['REQUEST_METHOD']) {
+            case 'GET':
+                $sensor->index();
+                break;
+
+            case 'POST':
+                $sensor->store($_POST);
+                break;
+
+            case 'PUT':
+                $sensor->update($_POST);
+                break;
+
+            case 'DELETE':
+                $sensor->delete($_POST);
+                break;    
+        } 
+    },
+    "/leituras" => function() use ($leitura) {
+        verificaMetodo(["GET", "POST", "PUT", "DELETE"]);
+        
+        switch($_SERVER['REQUEST_METHOD']) {
+            case 'GET':
+                $leitura->index();
+                break;
+
+            case 'POST':
+                $leitura->store($_POST);
+                break;
+
+            case 'PUT':
+                $leitura->update($_POST);
+                break;
+
+            case 'DELETE':
+                $leitura->delete($_POST);
+                break;    
+        } 
     },
 ];
